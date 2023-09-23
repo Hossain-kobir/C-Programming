6824e8732b7a28a7adce74f868cf85b3c8b88a16
Hossain-kobir <hossainkobir37bd@gmail.com>
2023-09-23 17:26:28 +0000

    Sep 23, 2023, 11:26 PM

--- Data Structure/Week 5 Binary Tree/Assignment 4 Exam M 20/Sum_of_tree.cpp
+++ Data Structure/Week 5 Binary Tree/Assignment 4 Exam M 20/Sum_of_tree.cpp
@@ -11,7 +11,7 @@ class Node{
             this->right=NULL;
         }
 };
-level_order_input_trtee()
+Node* level_order_input_tree()
 {
     int val;
     cin>>val;
@@ -31,13 +31,41 @@ level_order_input_trtee()
         Node* right;
         if(l == -1) left =NULL;
         else left = new Node(l);
+
         if(r == -1) right = NULL;
         else right = new Node(r);
+
+        p->left= left;
+        p->right= right;
+
         if(p->left) q.push(p->left);
         if(p->right) q.push(p->right);
     }
+    return root;
+}
+void level_order__print(Node* root)
+{
+        if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+    queue <Node* >q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node* p=q.front();
+        q.pop();
+
+        cout<<p->val<<endl;
+
+        if(p->left)q.push(root);
+        if(p->right)q.push(root);
+    }
+
 }
 int main()
 {
+    level_order_input_tree();
     return 0;
 }
