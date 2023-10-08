b3a45999c62a83213c7fadfcde81790dac0f3e4a
Hossain-kobir <hossainkobir37bd@gmail.com>
2023-10-08 09:42:27 +0000

    Oct 8, 2023, 3:42 PM

---
+++ Data Structure/Week 5 Binary Tree/Assignment 4 Exam M 20/Max_Min_Leaf_binary_tree.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public: int val;
+            Node* left;
+            Node* right;
+        Node(int val)
+        {
+            this->val=val;
+            this->left=NULL;
+            this->right=NULL;
+        }
+};
+vector<int> v;
+Node* level_order_input_tree()
+{
+    int val;
+    cin>>val;
+    Node*root;
+    if(val == -1) root = NULL;
+    else root = new Node(val);
+    queue<Node* >q;
+    if(root) q.push(root);
+    while (!q.empty())
+    {
+        
+        Node* p =q.front();
+        q.pop();
+        v.push_back(p->val);
+
+        int l, r;
+        cin>> l>>r;
+        Node* left;
+        Node* right;
+        if(l == -1) left =NULL;
+        else left = new Node(l);
+
+        if(r == -1) right = NULL;
+        else right = new Node(r);
+
+        p->left= left;
+        p->right= right;
+
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+    }
+    return root;
+}
+void level_order__print(Node* root)
+{
+        if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+    queue <Node* >q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node* p=q.front();
+        q.pop();
+
+       // cout<<p->val<<endl;
+
+        if(p->left)q.push(p->left);
+        if(p->right)q.push(p->right);
+    }
+
+}
+int maxLeaf = INT_MIN;
+int minLeaf = INT_MAX;
+
+void findMinMax (Node* root) 
+{
+    if (!root) return;
+
+    if (! root->left && ! root->right) 
+    { 
+        maxLeaf = max(maxLeaf, root->val);
+        minLeaf = min(minLeaf, root->val);
+    }
+    findMinMax(root->left);
+    findMinMax(root->right);
+}
+// int sum_of_tree(Node* root)
+// {
+//     if(root == NULL) return 0;
+//     //int sum = root->val;
+
+
+//     int l = sum_of_tree(root->left);
+//     int r = sum_of_tree(root->right);
+//     //return root->val+l+r;
+//     return max(l,r)+1;
+// }
+int main()
+{
+    Node* root = level_order_input_tree();
+    //level_order_input_tree();
+   // cout<<sum_of_tree(root)<<endl;
+    level_order__print(root);
+    
+    findMinMax(root );
+
+    cout << maxLeaf << " " << minLeaf << endl;
+    return 0;
+}
