d7b86e40af83fe7734dcba0cba87c232044edcf3
Hossain-kobir <hossainkobir37bd@gmail.com>
2023-10-11 15:34:04 +0000

    Oct 11, 2023, 9:34 PM

--- Data Structure/Week 5 Binary Tree/Assignment 4 Exam M 20/print_level_node.cpp
+++ Data Structure/Week 5 Binary Tree/Assignment 4 Exam M 20/print_level_node.cpp
@@ -11,24 +11,45 @@ class Node{
             this->right=NULL;
         }
 };
-    void input_tree()
+    void print_level_order(Node* root,int x)
+    {   if(root == NULL) return ;
+        bool find = false;
+        queue<pair<Node* ,int>>q;
+        q.push({root,0});
+        while (!q.empty())
+        {
+            pair<Node*,int> pr=q.front();
+            Node* node= pr.first;
+            int level = pr.second;
+            q.pop();
+            if(level == x)
+            {
+                cout<<node->val<<" ";
+                find= true;
+            }
+            if(node->left)q.push({node->left,level+1});
+            if(node->right)q.push({node->right,level+1});
+        }
+        if(!find)cout<<"Invalid";
+    }
+    Node * input_tree()
     {
         int val;
         cin>>val;
         Node* root;
         if(val == -1)root = NULL;
-        else root == new Node(val);
+        else root = new Node(val);
         queue<pair<Node* ,int>>q;
         if(root) q.push({root,0});
         while(!q.empty())
         {
-            pair<Node* ,int> pr= q.front();
+            pair<Node* ,int> pr = q.front();
             Node* node= pr.first;
             int level =pr.second;
             q.pop();
             
             int l,r;
-            cin>>l,r;
+            cin>>l>>r;
             Node*left;
             Node* right;
             if(l== -1) left= NULL;
@@ -38,7 +59,7 @@ class Node{
                 q.push({node->left,level+1});
             }
 
-            if(r== -1) left= NULL;
+            if(r == -1) left= NULL;
             else{
                 left= new Node(l);
                 node->right=right;
@@ -49,6 +70,9 @@ class Node{
     }
 int main()
 {
-     input_tree();
+    Node* root= input_tree();
+    int x;
+    cin>>x;
+    print_level_order(root, x);
     return 0;
 }
